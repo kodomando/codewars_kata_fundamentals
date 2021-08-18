0958f5c4aea7470cf65f0f30efed3d4998f79768
kodomando <86344475+kodomando@users.noreply.github.com>
2021-08-18 16:06:10 +0900

    Update Printer Errors.py

--- Printer Errors.py
+++ Printer Errors.py
@@ -1,30 +1,14 @@
 '''my solution at top, very standard'''
-# def printer_error(s):
-# 	good_letters = [x for x in 'abcdefghijklm']
-# 	count = len(s)
-# 	errors = 0
-# 	for letters in s:
-# 		if letters not in good_letters:
-# 			errors += 1
-# 	return f'{errors}/{count}'
-#
-# printer_error('aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz')
+def printer_error(s):
+ 	good_letters = [x for x in 'abcdefghijklm']
+ 	count = len(s)
+ 	errors = 0
+ 	for letters in s:
+ 		if letters not in good_letters:
+ 			errors += 1
+return f'{errors}/{count}'
 
-
-'''hard to read solution'''
-# def printer_errors(s):
-# 	'''
-# 	create a list and append it with each results of
-# 	(for x in s) if (x doesn't exist in iterations of string 'abcdefghijklm')
-#
-# 	splits 'input string' to individual character strs and iterates over each
-# 	compares to the str chrs of 'abcdefghijklm'. if input str char doesn't exist
-# 	in 'abcd..' append temporary list.
-#
-# 	Get count of both 'error' characters and total length of str chrs in input string.
-# 	use string formatting to display values errors/total
-# 	'''
-# 	return "{}/{}".format(len([x for x in s if x not in 'abcdefghijklm']), len(s))  #hella readable this one
+ printer_error('aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz')
 
 
 '''second solution'''
@@ -38,3 +22,18 @@ def printer_error(s):
         if i > "m": #cleverrr
             errors += 1
     return str(errors) + "/" + str(count)
+
+'''kinda of hard to read solution, this one was really cool
+# def printer_errors(s):
+# 	'''
+# 	create a list and append it with each results of
+# 	(for x in s) if (x doesn't exist in iterations of string 'abcdefghijklm')
+#
+# 	splits 'input string' to individual character strs and iterates over each
+# 	compares to the str chrs of 'abcdefghijklm'. if input str char doesn't exist
+# 	in 'abcd..' append temporary list.
+#
+# 	Get count of both 'error' characters and total length of str chrs in input string.
+# 	use string formatting to display values errors/total
+# 	'''
+# 	return "{}/{}".format(len([x for x in s if x not in 'abcdefghijklm']), len(s))  #hella readable this one!
