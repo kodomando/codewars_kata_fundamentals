728e5ad3c071a104ed24ccc764e7ed4570e1e54e
kodomando <86344475+kodomando@users.noreply.github.com>
2021-08-18 16:21:11 +0900

    Update Dubstep.py

--- Dubstep.py
+++ Dubstep.py
@@ -1,3 +1,43 @@
+'''
+Polycarpus works as a DJ in the best Berland nightclub, 
+and he often uses dubstep music in his performance. Recently, 
+he has decided to take a couple of old songs and make dubstep 
+remixes from them.
+
+Let's assume that a song consists of some number of words 
+(that don't contain WUB). 
+To make the dubstep remix of this song, Polycarpus inserts a 
+certain number of words "WUB" before the first word of the song 
+(the number may be zero), after the last word (the number may be zero),
+and between words (at least one between any pair of neighbouring words), 
+and then the boy glues together all the words, including "WUB",
+in one string and plays the song at the club.
+
+For example, a song with words "I AM X" can transform into a 
+dubstep remix as "WUBWUBIWUBAMWUBWUBX" and cannot transform
+into "WUBWUBIAMWUBX".
+
+Recently, Jonny has heard Polycarpus's new dubstep track, 
+but since he isn't into modern music, he decided to find out 
+what was the initial song that Polycarpus remixed. 
+Help Jonny restore the original song.
+
+Input
+The input consists of a single non-empty string, 
+consisting only of uppercase English letters, 
+the string's length doesn't exceed 200 characters
+
+Output
+Return the words of the initial song that Polycarpus 
+used to make a dubsteb remix. Separate the words with a space.
+
+Examples
+song_decoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB")
+  # =>  WE ARE THE CHAMPIONS MY FRIEND
+  
+'''
+
+
 song = 'WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB'
 songz = 'WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB'
 songz2 = 'AWUBBWUBC'
@@ -19,85 +59,3 @@ for stuff in song_list:
 	song_decoder(eval(stuff))
 
 
-# def song_decoder(song):
-#     song = song.split('WUB')
-#     print(song)
-#     original_song = []
-#     for i in song:
-#         if i != '':
-#             original_song += [i]
-#     return ' '.join(original_song)
-#
-#
-# print(song_decoder(song))
-
-# def song_decoder(song):
-# 	song = song.replace('WUB', ' ')
-# 	print(song)
-# 	song = song.split()
-# 	print(song)
-# 	print(' '.join(song))
-#
-#
-# song_decoder(songz4)
-
-
-# def song_decoder(song):
-# 	answer = None
-# 	words = song.replace('WUB', ' ')
-# 	words = words.strip()
-# 	length = len(words)
-# 	print(words)
-# 	print('length:' ,length)
-#
-# 	count = 0
-# 	for i, string in enumerate(words):
-# 		if string == ' ':
-# 			count += 1
-# 			if count == 2:
-# 				print(words[i])
-# 				words.replace(words[i], '')
-# 				count = 0
-#
-#
-# 	print(words)
-	# return words
-
-
-
-# song_decoder(songz2)
-# song_decoder(songz3)
-# song_decoder(songz4)
-
-
-
-
-
-
-
-# def song_decoder(song):
-#     answer = None
-#     words = song.replace('WUB', ' ')
-#     words = words.strip(' ')
-#     length = len(words)
-#
-#     j = 0
-#     if length % 3 == 0:
-#         j = -1
-#     if length % 3 != 0:
-#         j = length - 1
-#
-#     for i, string in enumerate(words[:j]):
-#         if words[i] + words[i + 1] == '  ' or words[i] + words[i - 1] == '  ':
-#              answer = words.replace('  ', ' ')
-#
-#     return answer
-
-
-# for i, string in enumerate(words[:]):
-# 	# if words[i - 1] == None:
-# 	# 	answer = words
-# 	if words[i] + words[i - 1] == '  ':
-# 		answer = words.replace('  ', ' ')
-# 	else:
-# 		answer = words
